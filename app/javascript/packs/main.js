7e0b51f0d051c4662753872908d1725d329d9f85
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-10-25 15:09:59 +0800

    add new poem js function

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,11 +1,12 @@
 document.addEventListener("turbolinks:load", () => {
 
-   let timeEl = document.querySelector('#time');
-   let locationEl = document.querySelector('#location')
+   let 
+      timeEl = document.querySelector('#time'),
+      locationEl = document.querySelector('#location'),
+      checkTime = function() {
+         setInterval(() => timeEl.innerHTML = new Date().toLocaleTimeString(),1000);
+      }
 
-   let checkTime = function() {
-      setInterval(() => timeEl.innerHTML = new Date().toLocaleTimeString(),1000);
-   }
    checkTime();
 
    const regionNames = new Intl.DisplayNames(
@@ -24,9 +25,8 @@ document.addEventListener("turbolinks:load", () => {
    )
 
    
-   let poemUrl = 'http://127.0.0.1:3000/poems/2.json';
 
-   function fetchPoem() {
+   function fetchPoem(poemUrl) {
          return fetch(poemUrl).then(function(response) {
             return response.json();
          }).then(function(json) {
@@ -34,10 +34,88 @@ document.addEventListener("turbolinks:load", () => {
          });
    }
 
-   fetchPoem().then(function(result) {
-         console.log(result.title, result.body);
+   // fetchPoem().then(function(result) {
+   //       console.log(result.title, result.body);
+   // });
+
+   function addNewPoem(poemUrl, newPoemTitle) {
+
+      fetchPoem(poemUrl, newPoemTitle).then(function(result) {
+
+         const forkContainer = document.createElement('div');
+         const newTitleEl = document.createElement('p');
+         const newBodyEl = document.createElement('div');
+         let newBodyContent = result.body;
+
+         newTitleEl.innerHTML = newPoemTitle;
+         newTitleEl.style.marginBottom = '12px';
+         newBodyEl.insertAdjacentHTML("afterbegin", newBodyContent);
+         forkContainer.appendChild(newTitleEl);
+         forkContainer.appendChild(newBodyEl);
+         forkContainer.style.width = '33vw';
+
+         allForksContainer.appendChild(forkContainer);
+
+         let forksContainerPos = window.getComputedStyle(allForksContainer).left;
+
+         allForksContainer.style.left = `calc(${forksContainerPos} - 33vw`;
+
+
+   });
+      
+   }
+
+
+   let allForksLinks = document.querySelectorAll('#js-forks-container a');
+
+   let 
+   allForksContainer = document.querySelector('#js-forks-container');
+
+
+   allForksContainer.addEventListener('click', function(event) {
+      if(event.target && event.target.nodeName == "A") {
+
+         event.preventDefault();
+
+         let 
+         forkID = event.target.getAttribute('href'), 
+         poemUrl = `http://127.0.0.1:3000/poems/${forkID}.json`,
+         newPoemTitle = event.target.innerText;
+
+         addNewPoem(poemUrl, newPoemTitle);
+
+
+      }
    });
 
+
+
+
+
+   // forksClick.addEventListener('click', function() {
+
+   //    fetchPoem().then(function(result) {
+
+   //       const newDiv = document.createElement('div');
+   //       const newTitleEl = document.createElement('p');
+   //       const textContent = result.title;
+
+   //       newTitleEl.innerHTML = textContent;
+   //       newDiv.appendChild(newTitleEl);
+   //       newDiv.style.width = '33vw';
+
+   //       forksContainer.appendChild(newDiv);
+
+   //       let forksContainerPos = window.getComputedStyle(forksContainer).left;
+
+   //       forksContainer.style.left = `calc(${forksContainerPos} - 33vw`;
+
+
+   // });
+
+   // })
+
+   
 
 
  })
