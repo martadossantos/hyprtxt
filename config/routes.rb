00ce1694b771dd6102138731308508c018c9c550
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-11-11 11:03:27 +0800

    change active admin routes to fix database hit load issue when pushing to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :poems 
